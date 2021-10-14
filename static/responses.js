b4a5dc0cb58efdbf5efc8ce4b75d2b0ce8faf401
YannickMyxe <shawn.froste.sf@gmail.com>
2021-10-14 17:48:20 +0200

    commando's naar lowercase + typefout aangepast

--- static/responses.js
+++ static/responses.js
@@ -1,4 +1,7 @@
 function getBotResponse(input) {
+    // alles word omgevormd naar lowercase
+    input = input.toLowerCase();
+
     if (input == "steen") {
         return "papier";
     } else if (input == "papier") {
@@ -7,13 +10,20 @@ function getBotResponse(input) {
         return "steen";
     }
 
+    if(input == "help")
+    {
+        return "Hier vind je een lijst van alle commando's: \n*help\n*Hallo";
+    }
+
     // Simple responses
-    if (input == "Hallo") {
-        return "Hallo daar!";
-    } else if (input == "Daag") {
+    if (input == "hallo") {
+        return "Hallo! Waarmee kan ik je helpen?";
+    } else if (input == "dag") {
         return "Ik spreek je later!";
-    } else if (input == "Waar kan ik het menu vinden?") {
+    } else if (input == "waar kan ik het menu vinden?") {
         return "Het menu is te vinden op: navigatie>home."
+    } else if (input == "ik hou van restaurant Multicultura!") {
+        return "Ik ook! Bedankt dat je zo van ons restaurant houd!"
     } else {
         return "Probeer iets anders te vragen!";
     }
