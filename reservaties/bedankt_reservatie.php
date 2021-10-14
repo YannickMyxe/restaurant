97b27374053a1fbae449c5f0f29dcdf3eb7b0626
YannickMyxe <shawn.froste.sf@gmail.com>
2021-10-14 16:16:59 +0200

    php yeah!

--- reservaties/bedankt_reservatie.php
+++ reservaties/bedankt_reservatie.php
@@ -7,52 +7,21 @@
 <html lang="en">
 <head>
 <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Thank you for contacting me</title>    
-    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Display&family=Roboto&display=swap" rel="stylesheet"> 
-
-    <link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css">
-    <link rel="stylesheet" href="/style/nav.css">
-    <link rel="stylesheet" href="/style/main.css">
-    <link rel="stylesheet" href="/style/contact.css">
 </head>
 <body>
-<header>
-        <div class="navcontainer">
-            <div class="logo">
-                <img src="https://via.placeholder.com/120x50" alt="logo">
-                <a href="./">Home page</a>
-            </div>
-            <nav>
-                <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/projects">Projects</a></li>
-                    <li><a href="/cv">CV</a></li>
-                    <li><a href="/blog">Blog</a></li>
-                    <li><a href="/contact" class="current-page">Contact</a></li>
-                </ul>
-            </nav>
-        </div>
-    </header>
 	<main>
 	<div class="thank-you-message">
 		
 	<?php
 		// Name sent in
 		if ($name) {
-			echo '<h1>Thank you</h1>';
-			echo '<p>Thank you ' . htmlentities($name). ', I will contact you back as soon as possible!</p><p>In the mean time go back to the home page or click on the navbar to browse my portfolio.</p>';
+			echo '<h1>Reservatie ontvangen</h1>';
+			echo '<p>Bedankt ' . htmlentities($name). ', we zullen u zo spoedig mogelijk contacteren!</p>';
 			}
-			// Age sent in
-			else if ($age) {
-				echo '<p>Thank you, ' . htmlentities($age). ' year old stranger</p>';
-			}
-			// Nothing sent in
 			else {
-				echo '<p>Thank you, stranger</p>';
+				echo '<p>Bedankt.</p>';
 			}
-			echo '<a class="button" href="/">Return to Home page</a>';
+			echo '<a class="button" href="../">Return to Home page</a>';
 			?>
 	</div>
 </main>
--- reservaties/index.php
+++ reservaties/index.php
@@ -23,12 +23,13 @@ try {
 
 $name = isset($_POST['name']) ? (string)$_POST['name'] : '';
 $message = isset($_POST['message']) ? (string)$_POST['message'] : '';
-$email = isset($_POST['email']) ? (string)$_POST['email'] : '';
-$findme = isset($_POST['radio-findme']) ? (string)$_POST['radio-findme'] : '';
+$amount = isset($_POST['aantal']) ? (string)$_POST['aantal'] : '1';
+$date = isset($_POST['datum']) ? (string)$_POST['datum'] : '';
+
 $msgName = '';
 $msgMessage = '';
-$msgEmail = '';
-$msgFindme = '';
+$msgAmount = '';
+$msgDate = '';
 
 // form is sent: perform formchecking!
 if (isset($_POST['btnSubmit'])) {
@@ -37,34 +38,31 @@ if (isset($_POST['btnSubmit'])) {
 
     // name not empty
     if (trim($name) === '') {
-        $msgName = 'Please fill in your name';
+        $msgName = 'Gelieve uw naam in te geven';
         $allOk = false;
     }
-
+    if (trim($amount) === '') {
+        $msgAmount = 'Gelieve het aantal personen mee te geven voor uw reservatie';
+        $allOk = false;
+    }
+    if (trim($date) === '') {
+        $msgDate = 'Gelieve een datum in te geven';
+        $allOk = false;
+    }
     if (trim($message) === '') {
-        $msgMessage = 'Please leave a message';
-        $allOk = false;
+        $message = "geen opmerking toegevoegd";
     }
-
-    if (trim($email) === '') {
-        $msgEmail = 'Please enter your email adress so I can contact you back';
-        $allOk = false;
-    }
-
-    if (trim($findme) === '') {
-        $msgFindme = 'Please select one of the following options';
-        $allOk = false;
-    }
+    
 
     // end of form check. If $allOk still is true, then the form was sent in correctly
     if ($allOk) {
         // build & execute prepared statement
-        $stmt = $db->prepare('INSERT INTO messages (sender, email, message, findme, added_on) VALUES (?, ?, ?, ?, ?)');
-        $stmt->execute(array($name, $email, $message, $findme, (new DateTime())->format('Y-m-d H:i:s')));
+        $stmt = $db->prepare('INSERT INTO reservaties (naam, aantal, datum, opmerking, added_on) VALUES (?, ?, ?, ?, ?)');
+        $stmt->execute(array($name, $amount, $date, $message, (new DateTime())->format('Y-m-d H:i:s')));
 
         // the query succeeded, redirect to this very same page
         if ($db->lastInsertId() !== 0) {
-            header('Location: formchecking_thanks.php?name=' . urlencode($name));
+            header('Location: bedankt_reservatie.php?name=' . urlencode($name));
             exit();
         } // the query failed
         else {
@@ -81,8 +79,6 @@ if (isset($_POST['btnSubmit'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Reservaties | Multicultura</title>
-    <link rel="stylesheet" href="/static/chat.css">
-    <link rel="stylesheet" href="/static/home.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css">
     <link rel="stylesheet" href="../css/main.css">
@@ -97,29 +93,31 @@ if (isset($_POST['btnSubmit'])) {
             <h1>Reservaties!</h1>
             <p>Welkom bij het reservatie menu, gelieve dit formulier in te vullen.</p>
             <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-                <label for="name" id="name">Jouw naam: </label>
-                <input name="name" id="name" class="text" type="text" value="<?php echo htmlentities($name); ?>">
-                <span class="message error"><?php echo $msgName; ?></span>
+                <div class="form-item">
+                    <label for="name" id="name">Jouw naam</label>
+                    <input name="name" id="name" class="text" type="text" maxlength="100" value="<?php echo htmlentities($name); ?>">
+                    <span class="message error"><?php echo $msgName; ?></span>
+                </div>
 
-                <label for="email">Emailadres: </label>
-                <input name="email" id="email" class="text" type="email" value="<?php echo htmlentities($email); ?>">
-                <span class="message error"><?php echo $msgEmail; ?></span>
+                <div class="form-item">
+                    <label for="aantal">Aantal personen</label>
+                    <input type="number" name="aantal" id="aantal" min="1" max="20" value="<?php echo htmlentities($amount); ?>">
+                    <span class="message error"><?php echo $msgAmount; ?></span>
+                </div>
 
-                
-                <label class="radiotitle">How did you find me?</label>
-                <input type="radio" id="friends" name="radio-findme" value="friends" <?php if(isset($_POST['radio-findme']) && $_POST['radio-findme'] === 'friends'){echo 'checked';}?>>
-                <label for="friends">Friends</label><br>
-                <input type="radio" id="social" name="radio-findme" value="socialmedia" <?php if(isset($_POST['radio-findme']) && $_POST['radio-findme'] === 'socialmedia'){echo 'checked';}?>>
-                <label for="social">Social Media</label><br>
-                <input type="radio" id="google" name="radio-findme" value="google" <?php if(isset($_POST['radio-findme']) && $_POST['radio-findme'] === 'google'){echo 'checked';}?>>
-                <label for="google">Google</label>
-                <span class="message error"><?php echo $msgFindme; ?></span>
-                <p>
-                    <button type="submit" id="btnSubmit" name="btnSubmit">Send your message</button>
-                </p>
-                <label for="message">Heb je nog opmerkingen?</label>
-                <textarea name="message" id="message" class="text"><?php echo htmlentities($message); ?></textarea>
-                <span class="message error"><?php echo $msgMessage; ?></span>
+                <div class="form-item">
+                    <label for="datum">Datum reservatie</label>
+                    <input type="datetime-local" name="datum" id="datum" value="<?php echo htmlentities($date); ?>">
+                    <span class="message error"><?php echo $msgDate; ?></span>
+                </div>
+
+                <div class="form-item">
+                    <label for="message">Heb je nog opmerkingen?</label>
+                    <textarea name="message" id="message" class="text"><?php echo htmlentities($message); ?></textarea>
+                    <span class="message error"><?php echo $msgMessage; ?></span>
+                </div>
+
+                <p><button type="submit" id="btnSubmit" name="btnSubmit">Send your message</button></p>
             </form>
         </div>
     </main>
