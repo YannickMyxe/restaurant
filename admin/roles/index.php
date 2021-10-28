c1c6e16a25e4e75a81f16cee451e2d50407feb5c
YannickMyxe <shawn.froste.sf@gmail.com>
2021-10-28 14:22:07 +0200

    Roles functioneel

--- admin/roles/index.php
+++ admin/roles/index.php
@@ -4,7 +4,7 @@ session_start();
 
 // Check if the user is already logged in, if yes then redirect him to welcome page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] === false){
-    header("location: ../../");
+    header("location: ../../login");
     exit;
 }
 // Show all errors (for educational purposes)
@@ -27,25 +27,26 @@ if (isset($_POST['gotoid']) && $_POST['gotoid'] !== 0){
 
 $id = (isset($_GET["id"]))? $_GET["id"] : 0;
 
-/* Attempt to connect to MySQL database */
-try{
-    $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
-    // Set the PDO error mode to exception
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e){
-    die("ERROR: Could not connect. " . $e->getMessage());
-}
+if (!isset($_POST['naam'])){
+    /* Attempt to connect to MySQL database */
+    try{
+        $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
+        // Set the PDO error mode to exception
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch(PDOException $e){
+        die("ERROR: Could not connect. " . $e->getMessage());
+    }
 
-// Validate credentials
-if(empty($username_err) && empty($password_err)){
-    // Prepare a select statement
-    $stmt = $pdo->prepare("SELECT * FROM accounts WHERE id=?");
-    $stmt->execute([$id]); 
-    $user = $stmt->fetch();
+    // Validate credentials
+    if(empty($username_err) && empty($password_err)){
+        // Prepare a select statement
+        $stmt = $pdo->prepare("SELECT * FROM accounts WHERE id=?");
+        $stmt->execute([$id]); 
+        $user = $stmt->fetch();
+    }
+    // Close connection
+    unset($pdo);
 }
-// Close connection
-unset($pdo);
-
 
 // #get roles
 $data = '';
@@ -67,6 +68,51 @@ if(empty($username_err) && empty($password_err)){
 }
 // Close connection
 unset($pdo);
+
+$naam = isset($_POST['naam']) ? (string)$_POST['naam'] : $user['username'];
+$id = isset($_POST['id']) ? (string)$_POST['id'] : $user['id'];
+
+$msgError = "";
+
+if(isset($_POST['btnSubmit'])){
+    if(!empty($_POST['roles'])) {
+        $selected = $_POST['roles'];
+        echo 'You have chosen: ' . $selected;
+    } else {
+        echo 'Please select the value.';
+    }
+    $allOk = true;
+    if(empty($_POST['roles']))
+    {
+        $msgError = "Er is een fout gebeurd bij roles, gelieve een role te kiezen";
+        $allOk = false;
+    }
+
+    if($allOk)
+    {
+        try{
+            $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
+            // Set the PDO error mode to exception
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e){
+            die("ERROR: Could not connect. " . $e->getMessage());
+        }
+        // build & execute prepared statement
+        $stmt = $pdo->prepare('INSERT INTO rolelink (accountID, RoleID) VALUES (?, ?)');
+        $stmt->execute(array($id, $selected));
+
+        // the query succeeded, redirect to this very same page
+        if ($pdo->lastInsertId() !== 0) {
+            header('Location: ../');
+            exit();
+        } // the query failed
+        else {
+            echo 'Databankfout.';
+            exit;
+        }
+    }
+
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -135,24 +181,25 @@ unset($pdo);
             <h1>Rollen</h1>
             <div class="">
                 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-                    <label for="id">ID</label><input name="id" type="number" readonly value="<?php echo htmlentities($user['id']);?>"></p>
-                    <label for="naam">Naam</label><input name="naam"  type="text" readonly value="<?php echo htmlentities($user['username']);?>"></p>
+                    <label for="id">ID</label><input name="id" type="number" readonly value="<?php echo htmlentities($id);?>"></p>
+                    <label for="naam">Naam</label><input name="naam"  type="text" readonly value="<?php echo htmlentities($naam);?>"></p>
                     <label for="role-select">Choose role</label>
                     <select name="roles" id="role-select">
-                        <option value="" disabled selected>--Please choose an option--</option>
+                        <option value="" disabled <?php if(!isset($_POST['roles'])) echo "selected"; ?>>--Please choose an option--</option>
                         <?php
                             foreach($data as $role){
                                 echo "<option value=".$role['id'].">".$role['name']."</option>";
                             }
                         ?>
                     </select>
+                    <span class="message error"><?php echo $msgError; ?></span>
                     <button type="submit" id="btnSubmit" name="btnSubmit" class="button">Voeg role toe</button>
                 </form>
             </div>
             <h2>Veranderen van ID </h2>
             <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
                 <label for="gotoid">ID</label><input type="number" name="gotoid" id="gotoid">
-                <button type="submit" id="btnSubmit" name="btnID" class="button">Verander naar id</button>
+                <button type="submit" id="btnSubmit-id" name="btnID" class="button">Verander naar id</button>
             </form>
             <p>
                 <a class="button" href="./?id=<?php if($id == 1){echo 1;}else{echo ($id-1);} ?>"> << vorige </a> 
